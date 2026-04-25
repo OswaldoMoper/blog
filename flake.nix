1ad4c6ff706380acf6a09e9659edc5d90661a332
Oswaldo Moper <oswaldomoyap@gmail.com>
2026-04-25 01:29:25 +0000

    Use haskell-flake instead, add config to vscode

--- flake.nix
+++ flake.nix
@@ -2,98 +2,84 @@
   description = "Hamlet-free Lucid Yesod app";
 
   inputs = {
-    flake-compat = {
-      url = "github:edolstra/flake-compat";
-      flake = false;
-    };
-    haskellNix.url = "github:input-output-hk/haskell.nix";
-    nixpkgs.follows = "haskellNix/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    haskell-flake.url = "github:srid/haskell-flake";
   };
 
   outputs = inputs@{ self
                    , nixpkgs
-                   , flake-utils
-                   , haskellNix
-                   , flake-compat
+                   , flake-parts
+                  , ...
                    }:
     let
-      overlays =
-        [ haskellNix.overlay
-          (final: prev:
-            let
-              devEnv = ''
-                export YESOD_PORT=3000
-              '';
-            in {
-              blog = final.haskell-nix.cabalProject {
-                src = final.haskell-nix.cleanSourceHaskell {
-                  src = ./.;
-                  name = "blog";
-                };
-                compiler-nix-name = "ghc966";
-                shell.tools = {
-                  cabal = {};
-                  haskell-language-server = {};
-                  ghcid = {};
-                  hlint = {};
-                  stylish-haskell = {};
-                };
-                shell.shellHook = devEnv;
+      devEnv = ''
+        export YESOD_PORT=3000
+        echo "DEV Port: $YESOD_PORT"
+      '';
+    in
+      flake-parts.lib.mkFlake { inherit inputs; } {
+        systems = [ "x86_64-linux" ];
+        imports = [ inputs.haskell-flake.flakeModule ];
+        perSystem = {self', system, lib, config, pkgs, ...}: {
+          haskellProjects.default = {
+            projectRoot = ./.;
+            basePackages = pkgs.haskell.packages.ghc96;
+            devShell = {
+              tools = hp: {
+                cabal = hp.cabal-install;
+                hlint = hp.hlint;
+                haskell-language-server = hp.haskell-language-server;
+                stylish-haskell = hp.stylish-haskell;
               };
-              blog-wrapper = pkgs.writeShellApplication {
-                name = "blog-wrapped";
-                runtimeInputs = [ self.packages.x86_64-linux.default ];
-                text = ''
-                  cd ~/blog
-                  touch src/Settings/StaticFiles.hs
-                  ${self.packages.x86_64-linux.default}/bin/blog
-                '';
-              };
-              blog-dev = pkgs.writeShellApplication {
-                name = "blog-dev";
-                runtimeInputs = final.lib.flatten [
-                  final.blog.shell.nativeBuildInputs
-                  final.blog.shell.buildInputs
+              hlsCheck.enable = false;
+              mkShellArgs = {
+                nativeBuildInputs = with pkgs; [
+                  haskell.packages.ghc96.yesod-bin
+                  postgresql
                 ];
-                text = ''
-                    ${devEnv}
-                    
-                    ghcid \
-                          --command '(echo ":l src/Application.hs" && cat) | cabal v2-repl' \
-                          --test appMain \
-                          --warnings --restart="src" --restart="app"
-                  '';
+                shellHook = devEnv;
               };
-            }
-          )
-        ];
-      pkgs = import nixpkgs {
-        system = "x86_64-linux";
-        inherit overlays;
-        inherit (haskellNix) config;
-      };
-      flake = pkgs.blog.flake{};
-      in
-        flake-utils.lib.eachSystem [ "x86_64-linux" ] (system: flake //{
-          packages = flake.packages // {
-            default = flake.packages."blog:exe:blog";
-            blog-wrapper = pkgs.blog-wrapper;
-            blog-dev = pkgs.blog-dev;
+            };
           };
-          apps = flake.apps // {
-            default = flake.apps."blog:exe:blog";
+
+          packages = {
+            default = config.haskellProjects.default.outputs.packages.blog.package;
+            blog-wrapper = pkgs.writeShellApplication {
+              name = "blog-wrapped";
+              runtimeInputs = [ self'.packages.default ];
+              text = ''
+                cd ~/blog
+                touch src/Settings/StaticFiles.hs
+                # ${self.packages.default}/bin/blog
+                blog
+              '';
+            };
+            blog-dev = pkgs.writeShellApplication {
+              name = "blog-dev";
+              runtimeInputs = (with config.haskellProjects.default.outputs.devShell; 
+                nativeBuildInputs ++ buildInputs
+              ) ++ [ pkgs.ghcid pkgs.cabal-install ];
+              text = ''
+                ${devEnv}
+                ghcid \
+                  --command '(echo ":l src/Application.hs" && cat) | cabal v2-repl' \
+                  --test appMain \
+                  --warnings --restart="src" --restart="app"
+              '';
+            };
+          };
+          apps = {
+            default = config.haskellProjects.default.outputs.apps.blog;
             dev = {
               type = "app";
-              program = "${pkgs.blog-dev}/bin/blog-dev";
+              program = "${self'.packages.blog-dev}/bin/blog-dev";
             };
           };
           legacyPackages = pkgs;
-        });
-  # --- Flake Local Nix Configuration ----------------------------
+        };
+      };
   nixConfig = {
-    extra-substituters = ["https://cache.iog.io"];
-    extra-trusted-public-keys = ["hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="];
-    allow-import-from-derivation = "true";
+    allow-import-from-derivation = true; 
   };
 }
